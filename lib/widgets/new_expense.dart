2b23669ce286da055837943d0ec5280dca4fbb4c
Leona <lonaverbovci@gmail.com>
2023-11-24 22:58:04 +0100

    added Bottomm Sheet Menu anda text field

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+    // it's called automatically bu flutter when the widget & its state are about to be removed from the UI
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                },
+                child: Text('Save Expense'),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
